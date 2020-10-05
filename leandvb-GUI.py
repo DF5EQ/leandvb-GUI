fbcb88b1bd5889e5ae938d509437f1c502ce78f7
Peter Bägel (DF5EQ) <peter@baegel.de>
2020-10-05 21:23:07 +0200

    relevant TODO deleted

--- leandvb-GUI.py
+++ leandvb-GUI.py
@@ -8,7 +8,6 @@
 # Leandvb by F4DAV (github leansdr)
 # Wrapper by pe2jko@540.org
 
-# TODO reorganize dropdown selections
 # TODO leandvb-run as function like leandvb-stop
 # TODO leandvb: --tune is broken, use --derotate instead
 # TODO streamline usage of viewer ffplay and mplayer
