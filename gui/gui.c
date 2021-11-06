e3602fe043f528aa87b112565d623b6e9472e520
Peter Bägel (DF5EQ) <peter@baegel.de>
2021-11-06 20:22:56 +0100

    lnblo entry populated with data from code

--- gui/gui.c
+++ gui/gui.c
@@ -20,6 +20,12 @@ static GtkBuilder*      builder;
 static GtkCssProvider*  css_provider;
 static GtkWidget*       window;
 static GtkComboBoxText* fec_combobox;
+static GtkEntry*        lnblo_entry;
+
+/* data for entries */
+static gfloat lnblo_value         = 0.0;
+static gfloat lnblo_value_default = 9750.000;
+static gchar  lnblo_string[11]    = "";
 
 /* data for comboboxes */
 static gchar* fec_list[]       = { "1/2", "2/3", "3/4", "4/5", "5/6", "6/7", "7/8" };
@@ -70,8 +76,13 @@ void gui_init (void)
     /* expose needed widgets */
     window       = GTK_WIDGET         (gtk_builder_get_object (builder, "main_window") );
     fec_combobox = GTK_COMBO_BOX_TEXT (gtk_builder_get_object (builder, "fec_combobox"));
+    lnblo_entry  = GTK_ENTRY          (gtk_builder_get_object (builder, "lnblo_entry") );
     
-    /* populate comboboxes with data */
+    /* populate entries */
+    sprintf (lnblo_string, "%6.3f", lnblo_value_default);
+    gtk_entry_set_text (lnblo_entry, lnblo_string);
+
+    /* populate comboboxes */
     for( i=0; i<sizeof(fec_list)/sizeof(gchar*); i++ )
     {
         gtk_combo_box_text_append_text (fec_combobox, fec_list[i]);
