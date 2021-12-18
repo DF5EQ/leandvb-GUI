1d08d5da8528597bff09a7499834f3ba36bcf326
Peter Bägel (DF5EQ) <peter@baegel.de>
2021-12-18 19:56:26 +0100

    parameters file as command line argument

--- main/main.c
+++ main/main.c
@@ -27,11 +27,24 @@
 
 int main (int argc, char *argv[])
 {
+    const char* param_file;
+
     gtk_init (&argc, &argv);
     setlocale(LC_ALL, "en_US.UTF-8");
-    param_init("parameters.json");
+
+    if (argc > 1)
+    {
+        param_file = argv[1];
+    }
+    else
+    {
+        param_file = "parameters.json";
+    }
+    param_init(param_file);
+
     gui_init ();
     gtk_main ();
+
     param_deinit();
 
   return 0;
